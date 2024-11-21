a4128d3c19b94cb2e40c6023f61afb6fe705c6ce
Rays Coding <gautamamit557@gmail.com>
2024-11-21 22:07:54 +0530

    Counter

--- react-projects/src/App.jsx
+++ react-projects/src/App.jsx
@@ -1,12 +1,7 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
 import Counter from "./counter"
 
 function App() {
   return (
-    
     <Counter />
   )
 }
